0a5e875ee4ecae4437d94f227cab65c6e2752c42
sheng <sheng@163.com>
2019-01-26 20:56:50 +0800

    添加主方法后

--- test/src/cn/itcast/domain/User.java
+++ test/src/cn/itcast/domain/User.java
@@ -2,10 +2,15 @@ package cn.itcast.domain;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
-    private String name ;
-    private int age ;
-    public void test(){
+public class User implements Serializable {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    public static void test() {
         System.out.println("这是一个测试工程");
     }
 
