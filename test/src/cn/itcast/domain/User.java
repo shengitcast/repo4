7784b00cd819be8545b840bc4be997194ef9d46a
sheng <sheng@163.com>
2019-01-26 20:02:42 +0800

    第一次修改后添加了一个方法

--- test/src/cn/itcast/domain/User.java
+++ test/src/cn/itcast/domain/User.java
@@ -5,4 +5,7 @@ import java.io.Serializable;
 public class User implements Serializable{
     private String name ;
     private int age ;
+    public void test(){
+        System.out.println("这是一个测试工程");
+    }
 }
